d6007695eabfc619fbc1c67e662f9a5f2a19a62c
susshikan <muhamadh123g@gmail.com>
2025-08-27 13:02:13 +0700

    add pws url

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/muhammad.haikal42/footballnews"]
 
 
 # Application definition
